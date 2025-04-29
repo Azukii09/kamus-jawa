839781c6aa627a9a1d4fe59603aa4bb1157e5d42
alvillage <azuknisianta@gmail.com>
2025-04-29 09:28:17 +0700

    first setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
 export default function Home() {
   return (
-    <div className={"flex w-full h-screen border-[1px]"}>
-      <div className={"flex w-3/12 h-full border-[1px] "}>a</div>
-      <div className={"flex w-6/12 h-full border-[1px]"}>b</div>
-      <div className={"flex w-3/12 h-full border-[1px]"}>c</div>
-    </div>
+    <main>konten</main>
   );
 }
