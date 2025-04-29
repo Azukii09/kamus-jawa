ca9c1eed24edccea52144fc53bb5de136e9b92ed
alvillage <azuknisianta@gmail.com>
2025-04-29 18:26:51 +0700

    ## - Add MobileMenu component and update layout structure -
    Introduce a new `MobileMenu` component to handle responsive navigation. Adjust the layout structure to integrate `MobileMenu` and improve visual consistency by refining styles and class usage.

---
+++ app/_component/MobileMenu/MobileNav.tsx
@@ -0,0 +1,5 @@
+export default function MobileMenu() {
+    return (
+        <div className={"flex w-full border-y-[1px]"}>navbar</div>
+    );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import React from "react";
 import LeftMenu from "@/app/_component/LeftMenu/LeftMenu";
 import RightMenu from "@/app/_component/RightMenu/RightMenu";
+import MobileMenu from "@/app/_component/MobileMenu/MobileNav";
 
 export const metadata: Metadata = {
   title: "Kamus Jawa",
@@ -17,12 +18,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`antialiased bg-slate-900 text-white`}
+        className={`antialiased bg-slate-900 text-slate-500`}
       >
-          <div className={"flex flex-col xl:flex-row w-full h-screen"}>
+          <div className={"flex flex-row w-full h-screen"}>
               <LeftMenu />
-              <div className={"flex xl:hidden w-full border-[1px] "}>navbar</div>
-              <div className={"flex w-full xl:w-6/12 h-full border-x-[1px]"}>
+              <div className={"flex flex-col w-full xl:w-6/12 h-full border-x-[1px]"}>
+                  <MobileMenu/>
                   {children}
               </div>
               <RightMenu/>
