43e28165f3ce82a0feb996f777cc61065419cdbb
alvillage <azuknisianta@gmail.com>
2025-04-30 12:38:53 +0700

    ## - Add UK and Indonesia flag icons with language toggle switch  -
    Introduced SVG components for UK and Indonesia flags and integrated them into the RightMenu component. Added a language toggle switch for seamless switching between languages, enhancing the user interface and accessibility.

--- app/_component/ui/RightMenu/RightMenu.tsx
+++ app/_component/ui/RightMenu/RightMenu.tsx
@@ -2,13 +2,21 @@ import React from "react";
 import {SideMenu} from "@/lib/data/Sidebar";
 import Link from "next/link";
 import DoorOutSvg from "@/app/_component/icon/DoorOut";
+import IndonesiaSVG from "@/app/_component/icon/Indonesia";
+import UKSVG from "@/app/_component/icon/UK";
 
 export default function RightMenu() {
     return (
         <section className={"hidden xl:flex w-3/12 h-full overflow-y-auto flex-col"}>
-            <div className={"w-full min-h-20 flex items-center justify-center gap-4 text-primary py-12"}>
-                <span>bahasa</span>
-                <span>bahasa</span>
+            <div
+                className={"w-full min-h-20 flex items-center justify-between gap-4 text-primary px-20 py-4"}>
+                <UKSVG className={"size-12"}/>
+                <input
+                    id="switch-link"
+                    type="checkbox"
+                    className="appearance-none relative inline-block rounded-full w-12 h-6 cursor-pointer before:inline-block before:absolute before:top-0 before:left-0 before:w-full before:h-full before:rounded-full before:bg-stone-200 before:transition-colors before:duration-200 before:ease-in after:absolute after:top-2/4 after:left-0 after:-translate-y-2/4 after:w-6 after:h-6 after:border after:border-stone-200 after:bg-primary after:rounded-full checked:after:translate-x-full after:transition-all after:duration-200 after:ease-in disabled:opacity-50 disabled:cursor-not-allowed dark:after:bg-primary checked:before:bg-stone-800 checked:after:border-stone-800"
+                />
+                <IndonesiaSVG className={"size-12"}/>
             </div>
             <div className={"w-full h-full flex flex-col items-center justify-between pb-8"}>
                 <ul className={"mt-6 h-full flex flex-col justify-between"}>
@@ -17,7 +25,7 @@ export default function RightMenu() {
                             <div className={"py-3 w-full"} key={index}>
                                 <Link
                                     href={"/"}
-                                    className={`${item.active? "bg-white text-primary hover:text-red-500":"hover:bg-white hover:text-red-500"} text-lg  py-2 px-6 inline-flex items-center w-full font-bold transition-colors duration-150 rounded-full`}
+                                    className={`${item.active ? "bg-white text-primary hover:text-red-500" : "hover:bg-white hover:text-red-500"} text-lg  py-2 px-6 inline-flex items-center w-full font-bold transition-colors duration-150 rounded-full`}
                                 >
                                     {item.icon}
                                     <span className="ml-4">{item.menuName}</span>
